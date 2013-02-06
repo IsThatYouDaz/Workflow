06d7bced319b4a89eea80f58e36fc11b3ec41e62
Darryl Taylor <darryl@blue-leaf.co.uk>
2013-02-06 09:06:05 +0000

    Applying changes to OSX

--- OSX/newdawnnewday.sh
+++ OSX/newdawnnewday.sh
@@ -13,8 +13,10 @@ function throwException {
 #main
 FULLNAME=`git config user.name`
 TODAY=`date +%A`
-YESTERDAY=`date -v -1d +%A`
+YESTERDAY=`date --date=yesterday +%A`
 CURRENT_BRANCH=`git branch | grep "*" | sed "s/* //"`
+IS_CLEAN=`git status | grep -c "nothing to commit"`
+
 
 for word in $FULLNAME; do
 	INITALS=$INITALS${word:0:1}
@@ -23,6 +25,19 @@ done
 TODAY_BRANCH_NAME=$INITALS"_"$TODAY
 YESTERDAY_BRANCH_NAME=$INITALS"_"$YESTERDAY
 
+IS_PUSHED=`git branch -a | grep -c $TODAY_BRANCH_NAME`
+
+if [ "$IS_CLEAN" == "0" ]; then
+	echo ' '
+	echo 'Changes outstanding to be commited, please commit and push these changes before running newdawnnewday again'
+	throwException
+fi
+
+if [ "$IS_PUSHED" == "1" ]; then
+	echo ' '
+	echo 'You have not pushed your daily branch, please push your daily branch and inform your merge manager'
+	throwException
+fi
 #if current branch isn't master then check it out
 #pull origin master
 #check if it completed successfully? some how
@@ -49,6 +64,24 @@ if [[ $pullSuccess != 0 ]] ; then
 fi
 
 echo 'Creating and checking out todays working branch'
+
+DAILY_EXISTS=`git branch | grep -c $TODAY_BRANCH_NAME`
+
+
+if [ "$DAILY_EXISTS" == "1" ] ; then
+	echo 'This daily branch already exists attempting cleanup'
+	git branch -d $TODAY_BRANCH_NAME
+	dailyRemoveSuccess=$?
+	if [[ $dailyRemoveSuccess != 0 ]] ; then
+		throwException
+	fi
+	git push origin :$TODAY_BRANCH_NAME
+	dailyOriginRemovalSuccess=$?
+	if [[ $dailyOriginRemovalSuccess != 0 ]] ; then
+		throwException
+	fi
+fi
+
 git checkout -b $TODAY_BRANCH_NAME
 checkoutDailySuccess=$?
 if [[ $checkoutDailySuccess != 0 ]] ; then
@@ -60,4 +93,15 @@ YESTERDAY_EXISTS=`git branch | grep -c $YESTERDAY_BRANCH_NAME`
 if [ "$YESTERDAY_EXISTS" == "1" ] ; then
 	echo 'Removing previous days branch'
 	git branch -d $YESTERDAY_BRANCH_NAME
+	yesterdayRemoveSuccess=$?
+	if [[ $yesterdayRemoveSuccess != 0 ]] ; then
+		throwException
+	fi
+	git push origin :$YESTERDAY_BRANCH_NAME
+	originRemovalSuccess=$?
+	if [[ $originRemovalSuccess != 0 ]] ; then
+		throwException
+	fi
 fi
+
+#modifed test
